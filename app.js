27bce4de2fa30a2bddfae71c8d8a31b258ecc01a
Felipe Nardi <felipenardi_92@hotmail.com>
2024-04-30 08:42:33 -0300

    included some security packages

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
+import { xss } from 'express-xss-sanitizer';
 import AppError from './src/utils/appError.js';
 import usersRouter from './src/routes/usersRouter.js';
 import booksRouter from './src/routes/booksRouter.js';
@@ -9,6 +13,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(bodyParser.json({ limit: '1kb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '1kb' }));
+app.use(xss());
 
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/books', booksRouter);
